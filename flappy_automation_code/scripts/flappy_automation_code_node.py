e3a2c5d984522661df2c540f2842dd9a7b7e31ec
Amaury <camusam@ethz.ch>
2020-01-23 17:19:48 +0100

    final touch

--- flappy_automation_code/scripts/flappy_automation_code_node.py
+++ flappy_automation_code/scripts/flappy_automation_code_node.py
@@ -126,14 +126,17 @@ class AutomatedFlappy():
             rate.sleep()
 
     def velCallback(self, msg):
-        # msg has the format of geometry_msgs::Vector3
-        # Example of publishing acceleration command on velocity velCallback
+        # We do the predict part of our filters, and update the states
         self.state.updateWithSpeedAndDt(msg.y, 1.0/30.0)
         self.state.vy = msg.y
         self.state.vx = msg.x
         self.firstObstacle.updateXPositionWithSpeedAndDt(msg.x, 1.0/30.0)
         self.secondObstacle.updateXPositionWithSpeedAndDt(msg.x, 1.0/30.0)
 
+        # When the first obstacle has been passed successfully, we replace the data 
+        # of the first by the data of the second, which becomes the first
+        # This is how the planner switches back to APPROACH mode
+        # A new second obstacle is ready to start estimating
         if self.firstObstacle.x.estimate < -0.25:
             self.firstObstacle.x = self.secondObstacle.x
             self.firstObstacle.gapHeightFilter = self.secondObstacle.gapHeightFilter
@@ -142,32 +145,36 @@ class AutomatedFlappy():
             self.secondObstacle.gapHeightFilter = GapTracker(
                 self.discretizationFactor, CEILING)
             self.secondObstacle.isSeen = False
+        
+        # Call the planner
         self.planner.plan()
 
-        # print "Y Position: {}".format(self.state.y)
-        # print "X position of first obstacle: {}".format(self.firstObstacle.x.estimate)
+        # Ask the controller for the acceleration commands
         (x, y) = self.controller.giveAcceleration()
+
+        # Publish these commands
         self.pub_acc_cmd.publish(Vector3(x, y, 0))
 
     def laserScanCallback(self, msg):
-        # msg has the format of sensor_msgs::LaserScan
-        # print laser angle and range
+        # Give the initial estimate using the bottom ray at the very beginning
         if not self.hasSetInitialState:
             length = msg.ranges[0]
             angle = msg.angle_min
             distanceToGround = math.fabs(math.sin(angle) * length)
             self.state.setInitialState(distanceToGround)
             self.hasSetInitialState = True
+        
+        # Then, sort the ray and use them to determine the obstacle distance and the height of the gap
         for i in range(len(msg.intensities)):
             hasHit = bool(msg.intensities[i])
             length = msg.ranges[i]
             angle = msg.angle_min + i * msg.angle_increment
 
+            # Computing distances from Flappy to hit point for X and Y
             height = math.sin(angle) * length
             distance = math.cos(angle) * length
 
             if hasHit:
-
                 if (height < 0.1 - self.state.y):
                     # Then it touches the ground so we ignore it
                     # print("ray %d touches the ground" % i)
@@ -178,8 +185,9 @@ class AutomatedFlappy():
                     # print("ray %d touches the ceiling" % i)
                     pass
                 else:
+                    # When it hits, it can be on the first or on the second obstacle
                     if distance < 0.5 + self.firstObstacle.x.estimate:
-                        # Update obstacle distance
+                        # Update 1st obstacle distance
                         self.firstObstacle.updateXPosition(
                             distance + OBSTACLE_WIDTH/2.0, 1.0)
                         self.firstObstacle.isSeen = True
@@ -190,29 +198,35 @@ class AutomatedFlappy():
                         self.firstObstacle.updateGapPosition(
                             heightOfTheHit, 10 * self.firstObstacle.x.estimate * self.firstObstacle.x.estimate, True)
                     else:
+                        # Update second obstacle distance
                         self.secondObstacle.updateXPosition(
                             distance + OBSTACLE_WIDTH/2.0, 2.0)
                         self.secondObstacle.isSeen = True
+
                         # Update Gap tracker
                         heightOfTheHit = self.state.y + \
                             self.secondObstacle.x.estimate * math.tan(angle)
                         self.secondObstacle.updateGapPosition(
                             heightOfTheHit, 10 * self.secondObstacle.x.estimate * self.secondObstacle.x.estimate, True)
             else:
+                # If a ray misses, we first need to decide if it actually has crossed an obstacle
+                # So we compare the horizontal distance with the first obstacle distance
                 if self.firstObstacle.isSeen and distance > self.firstObstacle.x.estimate:
                     # Then it sees through a gap
                     heightOfTheGap = self.state.y + \
                         self.firstObstacle.x.estimate * math.tan(angle)
                     self.firstObstacle.updateGapPosition(
                         heightOfTheGap, 10 * self.firstObstacle.x.estimate * self.firstObstacle.x.estimate, False)
-                    # print "Laser range: {}, angle: {}".format(msg.ranges[0], msg.angle_min)
+                
+                # And we then see if it had enough range to even go through the second obstacle
                 if self.secondObstacle.isSeen and distance > self.secondObstacle.x.estimate:
                     # Then it sees through a gap
                     heightOfTheGap = self.state.y + \
                         self.secondObstacle.x.estimate * math.tan(angle)
                     self.secondObstacle.updateGapPosition(
                         heightOfTheGap, 10 * self.secondObstacle.x.estimate * self.secondObstacle.x.estimate, False)
-                    # print "Laser range: {}, angle: {}".format(msg.ranges[0], msg.angle_min)
+            
+            # At the end of the pointcloud parsing, we signal the gap filters. See filters.py for more info
             self.firstObstacle.gapHeightFilter.endOfPointCloud()
             self.secondObstacle.gapHeightFilter.endOfPointCloud()
 
@@ -245,19 +259,21 @@ class AutomatedFlappy():
             new_array = obstacle.gapHeightFilter.voteArray - mini
             if diff != 0:
                 new_array /= float(diff)
-            # print(obstacle.gapHeightFilter.voteArray)
-            # print(new_array)
+
             plt.scatter(xObstacle, yObstacle,
                         c=cm.gist_yarg(new_array), edgecolor='none', label="obstacle")
 
+        # Plot in green the current best estimation of the gaps positions
         xGaps = [self.firstObstacle.x.estimate,
                   self.secondObstacle.x.estimate]
         yGaps = [self.firstObstacle.gapHeightFilter.estimate,
                   self.secondObstacle.gapHeightFilter.estimate]
+        plt.plot(xGaps, yGaps, "go", label="Gaps")
+
+        # Plot the position of Flappy in blue
         xFlappy = [0.0]
         yFlappy = [self.state.y] 
         plt.plot(xFlappy, yFlappy, "bo", label="Flappy")
-        plt.plot(xGaps, yGaps, "go", label="Gaps")
 
         plt.show(block=False)
         plt.pause(0.01)
